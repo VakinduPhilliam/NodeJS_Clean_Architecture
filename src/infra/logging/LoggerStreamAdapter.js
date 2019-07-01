b10a7c1512e4c85cb69724d1c03d9eaaf9917fe5
Vakindu Philliam <Pvakindu@gmail.com>
2019-07-01 18:14:48 +0300

    Add files via upload

---
+++ src/infra/logging/LoggerStreamAdapter.js
@@ -0,0 +1,21 @@
+const LoggerStreamAdapter = {
+  
+
+toStream(logger) {
+    
+
+return {
+ write(message) {
+ logger.info(message.slice(0, -1));
+ }
+ };
+  
+
+  }
+
+
+};
+
+
+
+module.exports = LoggerStreamAdapter;
