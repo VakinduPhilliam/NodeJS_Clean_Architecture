f23c82a9825f83c4b01d3346061cb3f361f7e3a0
Vakindu Philliam <Pvakindu@gmail.com>
2019-07-01 18:40:22 +0300

    Add files via upload

---
+++ src/interfaces/http/utils/createControllerRoutes.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+
+
+module.exports = function createControllerRoutes(controllerUri) {
+  
+
+const controllerPath = path.resolve('src/interfaces/http', controllerUri);
+  
+
+const Controller = require(controllerPath);
+
+  
+
+return Controller.router;
+
+
+};
