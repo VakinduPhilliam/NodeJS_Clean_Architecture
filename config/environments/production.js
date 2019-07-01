a5ffaa5357cf255db311b816d87713e01e2894b9
Vakindu Philliam <Pvakindu@gmail.com>
2019-07-01 18:00:14 +0300

    Add files via upload

---
+++ config/environments/production.js
@@ -0,0 +1,13 @@
+module.exports = {
+  
+
+web: {
+    port: process.env.PORT
+  },
+  
+logging: {
+    appenders: [
+      { type: 'console', layout: { type: 'basic' } }
+    ]
+  }
+};
---
+++ config/environments/test.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+const logPath = path.join(__dirname, '../../logs/test.log');
+
+
+
+module.exports = {
+  
+
+web: {},
+  
+
+logging: {
+    appenders: { cheese: { type: 'console', filename: logPath } },
+    categories: { default: { appenders: ['cheese'], level: 'error' } }
+  }
+};
+
