86e082be82502482667b14da7419a7bc88b51960
Vakindu Philliam <Pvakindu@gmail.com>
2019-07-01 18:45:57 +0300

    Add files via upload

---
+++ test/support/request.js
@@ -0,0 +1,9 @@
+const request = require('supertest-as-promised');
+
+const container = require('src/container');
+
+const server = container.resolve('server');
+
+
+
+module.exports = () => request(server.express);
