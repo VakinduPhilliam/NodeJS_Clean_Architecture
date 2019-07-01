f2dda97367e74c59da9082294ce996d0dfc762bc
Vakindu Philliam <Pvakindu@gmail.com>
2019-07-01 15:40:52 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const container = require('src/container');
+
+
+const app = container.resolve('app');
+
+
+
+app
+.start()
+.catch((error) => {
+    
+
+app.logger.error(error.stack);
+    
+process.exit();
+ 
+
+ });
